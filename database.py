fa891b13cba483b8f3270cbc5920971a39c483cd
Serhii <serhii.feniuk.23@pnu.edu.ua>
2026-05-29 09:12:40 +0300

    tasks completed

--- database.py
+++ database.py
@@ -7,16 +7,12 @@ SQLALCHEMY_DATABASE_URL = os.getenv(
     "postgresql://postgres:mysecretpassword@localhost:5432/library"
 )
 
-# Створюємо двигун (engine) для підключення
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
-# Створюємо фабрику сесій (через неї ми будемо робити запити до БД)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-# Базовий клас для всіх наших майбутніх моделей (таблиць)
 Base = declarative_base()
 
-# Функція-залежність (Dependency), яка буде видавати сесію для кожного запиту
 def get_db():
     db = SessionLocal()
     try:
